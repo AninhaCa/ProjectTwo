5a35175e020ee49931889f3b2a64ec75ce56a48b
Aninha <aninhapss2000@gmail.com>
2023-03-08 22:00:07 -0300

    Added custom XIB, image, label

---
+++ TwoProjectGitHub/TwoProjectGitHub/MyCustomXIBCell.swift
@@ -0,0 +1,29 @@
+//
+//  MyCustomXIBCell.swift
+//  TwoProjectGitHub
+//
+//  Created by Ana Paula Silva de Sousa on 08/03/23.
+//
+
+import UIKit
+
+class MyCustomXIBCell: UITableViewCell {
+
+    @IBOutlet var labelXIB: UILabel!
+    @IBOutlet var imageXIB: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setup() {
+        
+    }
+}
