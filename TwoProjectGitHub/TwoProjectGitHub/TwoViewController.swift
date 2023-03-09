ec7184941719720fbb39d21aa60d6fab883c7e1e
Aninha <aninhapss2000@gmail.com>
2023-03-08 21:45:29 -0300

    Added view controller two, image, label, button

---
+++ TwoProjectGitHub/TwoProjectGitHub/TwoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TwoViewController.swift
+//  TwoProjectGitHub
+//
+//  Created by Ana Paula Silva de Sousa on 08/03/23.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    @IBOutlet var labelOne: UILabel!
+    @IBOutlet var labelTwo: UILabel!
+    @IBOutlet var followTwo: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func back(_ sender: Any) {
+    }
+    @IBAction func follow(_ sender: Any) {
+    }
+}
