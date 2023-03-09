9561d57dadd4f71ca2068a107f27980bc5171408
Aninha <aninhapss2000@gmail.com>
2023-03-08 22:09:41 -0300

    Added NSObject, var, init

---
+++ TwoProjectGitHub/TwoProjectGitHub/Woman.swift
@@ -0,0 +1,24 @@
+//
+//  Woman.swift
+//  TwoProjectGitHub
+//
+//  Created by Ana Paula Silva de Sousa on 08/03/23.
+//
+
+import UIKit
+
+class Woman: NSObject {
+    var name: String
+    var descriptionWoman: String
+    var createdWoman: String
+    var info: String
+    var image: String
+    
+    init(name: String, descriptionWoman: String, createdWoman: String, info: String, image: String) {
+        self.name = name
+        self.descriptionWoman = name
+        self.createdWoman = createdWoman
+        self.info = info
+        self.image = image
+    }
+}
