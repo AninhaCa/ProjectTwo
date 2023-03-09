31fec4be7b7e47f6ebc2fc3adab0bf289e0f5ad1
Aninha <aninhapss2000@gmail.com>
2023-03-08 21:56:21 -0300

    Added custom cell, image, label

---
+++ TwoProjectGitHub/TwoProjectGitHub/MyCustomCell.swift
@@ -0,0 +1,29 @@
+//
+//  MyCustomCell.swift
+//  TwoProjectGitHub
+//
+//  Created by Ana Paula Silva de Sousa on 08/03/23.
+//
+
+import UIKit
+
+class MyCustomCell: UITableViewCell {
+
+    @IBOutlet var imageCell: UIImageView!
+    @IBOutlet var labelCell: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func setup() {
+        
+    }
+}
